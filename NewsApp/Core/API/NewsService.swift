4c4544a7f50017350a19e37e377353e1a24a3409
Adam Pilarski <adam.pilarski87@gmail.com>
2023-05-30 10:36:40 +0200

    Alamofire request handler

--- NewsApp/Core/API/NewsService.swift
+++ NewsApp/Core/API/NewsService.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Alamofire
 
 enum NewsServiceError: Error{
     case serverError(NewsError)
@@ -19,36 +20,62 @@ class NewsService {
                           completion: @escaping (Result<[News], NewsServiceError>)->Void) {
         guard let request = endpoint.request else { return }
         
-        URLSession.shared.dataTask(with: request) { data, resp, error in
-            if let error = error {
+        AF.request(request).responseData { response in
+            switch response.result {
+            case .success(let data):
+                if let resp = response.response, resp.statusCode != 200 {
+                    do {
+                        let newsError = try JSONDecoder().decode(NewsError.self, from: data)
+                        completion(.failure(.serverError(newsError)))
+                    } catch {
+                        completion(.failure(.unknown()))
+                        print(error.localizedDescription)
+                    }
+                } else {
+                    do {
+                        let decoder = JSONDecoder()
+                        let newsData = try decoder.decode(NewsArray.self, from: data)
+                        completion(.success(newsData.articles))
+                    } catch {
+                        completion(.failure(.decodingError()))
+                        print(error)
+                    }
+                }
+            case .failure(let error):
                 completion(.failure(.unknown(error.localizedDescription)))
-                return
             }
-            
-            if let resp = resp as? HTTPURLResponse, resp.statusCode != 200 {
-                do {
-                    let newsError = try JSONDecoder().decode(NewsError.self, from: data ?? Data())
-                    completion(.failure(.serverError(newsError)))
-                } catch let err {
-                    completion(.failure(.unknown()))
-                    print(err.localizedDescription)
-                }
-            }
-            
-            if let data = data {
-                do {
-                    let decoder = JSONDecoder()
-                    let newsData = try decoder.decode(NewsArray.self, from: data)
-                    completion(.success(newsData.articles))
-                } catch let err {
-                    completion(.failure(.decodingError()))
-                    print(err)
-                }
-            } else {
-                completion(.failure(.unknown()))
-            }
-            
-        }.resume()
+        }
+        
+//        URLSession.shared.dataTask(with: request) { data, resp, error in
+//            if let error = error {
+//                completion(.failure(.unknown(error.localizedDescription)))
+//                return
+//            }
+//
+//            if let resp = resp as? HTTPURLResponse, resp.statusCode != 200 {
+//                do {
+//                    let newsError = try JSONDecoder().decode(NewsError.self, from: data ?? Data())
+//                    completion(.failure(.serverError(newsError)))
+//                } catch let err {
+//                    completion(.failure(.unknown()))
+//                    print(err.localizedDescription)
+//                }
+//            }
+//
+//            if let data = data {
+//                do {
+//                    let decoder = JSONDecoder()
+//                    let newsData = try decoder.decode(NewsArray.self, from: data)
+//                    completion(.success(newsData.articles))
+//                } catch let err {
+//                    completion(.failure(.decodingError()))
+//                    print(err)
+//                }
+//            } else {
+//                completion(.failure(.unknown()))
+//            }
+//
+//        }.resume()
         
         
     }
